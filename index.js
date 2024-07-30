83fd8d22cbc5923d02aafcda1981926dc6671d06
Madeline Conway <conway.maddy@yahoo.com>
2024-07-30 15:30:45 -0400

    completed

---
+++ index.js
@@ -0,0 +1,33 @@
+number = 1
+function add (a,b) {
+return (a+b)
+}
+function subtract (a,b) {
+return (a-b)
+}
+function decrement (n) {
+n--
+return n 
+}
+
+function increment (n) {
+n++
+return n 
+}
+
+
+number = 3.4
+function multiply (a,b) {
+return (a*b)
+}
+function makeInt(n){
+return parseInt (n, 10)
+}
+
+number = 5.0
+function divide (a,b) {
+return (a/b)
+}
+function preserveDecimal(n){
+return parseFloat(n)
+}
